d400e0584660d20f4bdd479706ea142e74f61fa2
n9Mtq4 <bill.wresnahan@gmail.com>
2017-03-15 11:16:31 -0400

    Initial Commit

---
+++ src/main/java/me/willjake/hamlet/GameLauncher.java
@@ -0,0 +1,16 @@
+package me.willjake.hamlet;
+
+/**
+ * Created by will on 3/15/17 at 11:12 AM.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+public class GameLauncher {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The game has been launched.");
+		
+	}
+	
+}
