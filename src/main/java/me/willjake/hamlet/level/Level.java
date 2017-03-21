586c62effd3882a2a71b6eb8033b7e43d8884b12
TheLocust3 <jake.kinsella@gmail.com>
2017-03-21 14:39:16 -0400

    Add skeleton for level class

---
+++ src/main/java/me/willjake/hamlet/level/Level.java
@@ -0,0 +1,15 @@
+package me.willjake.hamlet.level;
+
+/**
+ * Created by jakekinsella on 3/21/17.
+ */
+public class Level {
+
+    public Level(String levelFile) {
+        this.setup(levelFile);
+    }
+
+    private void setup(String levelFile) {
+
+    }
+}
