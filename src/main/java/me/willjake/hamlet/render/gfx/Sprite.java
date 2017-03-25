3136f7520906d2ba24ac4a8b8601def46560045f
n9Mtq4 <bill.wresnahan@gmail.com>
2017-03-25 18:21:33 -0400

    sprite upscaling

--- src/main/java/me/willjake/hamlet/render/gfx/Sprite.java
+++ src/main/java/me/willjake/hamlet/render/gfx/Sprite.java
@@ -10,8 +10,8 @@ import java.awt.image.BufferedImage;
  */
 public class Sprite {
 	
-	public final int SIZE;
-	public final int SIZE_MASK;
+	public int SIZE;
+	public int SIZE_MASK;
 	public int x;
 	public int y;
 	public int width;
@@ -66,6 +66,39 @@ public class Sprite {
 			newPixels[i] = pixels[i - 2 * (i % width) + width - 1];
 		}
 		pixels = newPixels;
+	}
+	
+	// TODO: add scale as an argument instead
+	/**
+	 * @return this
+	 * */
+	public Sprite upscale() {
+		final int scale = 2;
+		pixels = resizePixels(pixels, width, height, width * scale, height * scale);
+		width = width * scale;
+		height = height * scale;
+		SIZE = SIZE * 2;
+		SIZE_MASK = SIZE - 1;
+		return this;
+	}
+	
+	// http://tech-algorithm.com/articles/nearest-neighbor-image-scaling/
+	private static int[] resizePixels(int[] pixels, int w1, int h1, int w2, int h2) {
+		int[] temp = new int[w2*h2] ;
+		// EDIT: added +1 to account for an early rounding problem
+		int x_ratio = (int)((w1<<16)/w2) +1;
+		int y_ratio = (int)((h1<<16)/h2) +1;
+		//int x_ratio = (int)((w1<<16)/w2) ;
+		//int y_ratio = (int)((h1<<16)/h2) ;
+		int x2, y2 ;
+		for (int i=0;i<h2;i++) {
+			for (int j=0;j<w2;j++) {
+				x2 = ((j*x_ratio)>>16) ;
+				y2 = ((i*y_ratio)>>16) ;
+				temp[(i*w2)+j] = pixels[(y2*w1)+x2] ;
+			}
+		}
+		return temp ;
 	}
 	
 	private void setColor(int color) {
