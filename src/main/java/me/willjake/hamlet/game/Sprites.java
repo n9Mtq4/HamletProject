e30fed6febba38287815e66da8b46d39e66b6251
n9Mtq4 <bill.wresnahan@gmail.com>
2017-03-25 11:46:59 -0400

    more sprites

--- src/main/java/me/willjake/hamlet/game/Sprites.java
+++ src/main/java/me/willjake/hamlet/game/Sprites.java
@@ -20,9 +20,18 @@ public class Sprites {
 	public static final Sprite voidTile = new Sprite(Screen.ABS_TILE_SIZE, new Color(215, 215, 215));
 	public static final Sprite wallTile = new Sprite(Screen.ABS_TILE_SIZE, new Color(100, 100, 100));
 	
+	public static final AnimatedSprite kingForward = getAnimatedSprite(0, 1, 3, 16, characters, 60);
+	public static final AnimatedSprite kingBackward = getAnimatedSprite(0, 4, 6, 16, characters, 60);
+	
+	// TODO: implement a standing vs walking in the player / animated sprite class
+	public static final Sprite ghostForwardStand = new Sprite(16, 0, 3, characters);
+	public static final Sprite ghostBackwardStand = new Sprite(16, 3, 3, characters);
+	public static final Sprite ghostLeftStand = new Sprite(16, 6, 3, characters); // TODO: draw a right or fix the damn flipY method
 	public static final AnimatedSprite ghostForward = getAnimatedSprite(3, 1, 3, 16, characters, 60);
 	public static final AnimatedSprite ghostBackward = getAnimatedSprite(3, 4, 6, 16, characters, 60);
 	public static final AnimatedSprite ghostLeft = getAnimatedSprite(3, 6, 8, 16, characters, 60);
+	
+	public static final Sprite guard = new Sprite(16, 6, 0, characters);
 	
 	/**
 	 * generates an animated sprite
