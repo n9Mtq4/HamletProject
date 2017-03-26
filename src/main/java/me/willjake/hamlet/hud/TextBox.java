a01490f153b6ffc488bd370532bc2f815f407dc8
TheLocust3 <jake.kinsella@gmail.com>
2017-03-25 20:50:22 -0400

    Speaker shows up on newline of textbox and is bolded

--- src/main/java/me/willjake/hamlet/hud/TextBox.java
+++ src/main/java/me/willjake/hamlet/hud/TextBox.java
@@ -14,66 +14,67 @@ import java.util.ArrayList;
  */
 public class TextBox {
 
-	private static final int BORDERX = 10;
-	private static final int BORDERY = 50;
+    private static final int BORDERX = 10;
+    private static final int BORDERY = 50;
     private static final int CHARACTERS_PER_LINE = 97;
     private static final int LINE_PADDING = 3; //px
-	private static final double LIFE_MODIFIER = 0.2; // % of a second for each character
+    private static final double LIFE_MODIFIER = 0.2; // % of a second for each character
+    private static final int FONT_SIZE = 14;
+    private static final String FONT_TYPE = Font.SANS_SERIF;
 
-	private boolean show = false;
-	private int life = 0;
-	private int xOff = 0;
-	private int yOff = 0;
-    private ArrayList<String> text;
+    private boolean show = false;
+    private int life = 0;
+    private int xOff = 0;
+    private int yOff = 0;
+    private ArrayList<String> text = new ArrayList<String>();
+    private boolean isName = false;
 
-	private Font font;
+    private Font font;
 
-	public TextBox() {
+    public TextBox() {
         this.font = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
     }
 
-	public void render(Screen screen) {
-		xOff = screen.xOff; // TODO: very bad
-		yOff = screen.yOff;
-	}
+    public void render(Screen screen) {
+        xOff = screen.xOff; // TODO: very bad
+        yOff = screen.yOff;
+    }
 
-	public void render(Graphics graphics) {
-		if (!show) return;
+    public void render(Graphics graphics) {
+        if (!show) return;
 
         this.drawTextBox(graphics);
         this.drawText(graphics);
-	}
-	
-	public void tick() {
-		life--;
-		
-		if (life <= 0) {
-			show = false;
-		}
-	}
-
-	public void go(String text, boolean bold) {
-	    if (bold) {
-            this.font = new Font(Font.SANS_SERIF, Font.BOLD, 14);
-        } else {
-            this.font = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
-        }
-
-        show = true;
-        this.text = this.splitTextIntoLines(text);
-        this.life = (int) (text.length() * LIFE_MODIFIER) * 100; // Totally random formula, pls ignore
     }
 
-    public void go(String text) {
-        go(text, false);
+    public void tick() {
+        life--;
+
+        if (life <= 0) {
+            show = false;
+        }
+    }
+
+    public void go(String text, String name) {
+        this.font = new Font(FONT_TYPE, Font.PLAIN, FONT_SIZE);
+
+        this.isName = name.length() > 0;
+        this.show = true;
+
+        if (this.isName) {
+            this.text.add(name + ":");
+        }
+
+        this.text.addAll(this.splitTextIntoLines(text));
+        this.life = (int) (text.length() * LIFE_MODIFIER) * 100; // Totally random formula, pls ignore
     }
 
     // Purposely ignores splitting words, sorta unnecessary
     private ArrayList<String> splitTextIntoLines(String text) {
-	    ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> lines = new ArrayList<String>();
 
         int charAt;
-	    for (charAt = 0; charAt < text.length() - CHARACTERS_PER_LINE; charAt += CHARACTERS_PER_LINE) {
+        for (charAt = 0; charAt < text.length() - CHARACTERS_PER_LINE; charAt += CHARACTERS_PER_LINE) {
             lines.add(text.substring(charAt, charAt + CHARACTERS_PER_LINE));
         }
 
@@ -84,7 +85,7 @@ public class TextBox {
         return lines;
     }
 
-	private void drawTextBox(Graphics graphics) {
+    private void drawTextBox(Graphics graphics) {
         FontRenderContext fontRenderContext = ((Graphics2D) graphics).getFontRenderContext();
 
         graphics.setColor(Color.WHITE);
@@ -94,7 +95,7 @@ public class TextBox {
     private void drawText(Graphics graphics) {
         FontRenderContext fontRenderContext = ((Graphics2D) graphics).getFontRenderContext();
 
-	    graphics.setColor(Color.BLACK);
+        graphics.setColor(Color.BLACK);
         graphics.setFont(this.font);
 
         int startY = BORDERX + 60;
@@ -102,14 +103,20 @@ public class TextBox {
             String string = this.text.get(i);
             int height = (int) this.getTextHeight(string, fontRenderContext);
 
-            graphics.drawString(string, BORDERX * 2, startY + (i * height));
+            if (this.isName && i == 0) {
+                graphics.setFont(new Font(FONT_TYPE, Font.BOLD, FONT_SIZE));
+                graphics.drawString(string, BORDERX * 2, startY + (i * height));
+                graphics.setFont(new Font(FONT_TYPE, Font.PLAIN, FONT_SIZE));
+            } else {
+                graphics.drawString(string, BORDERX * 2, startY + (i * height));
+            }
         }
     }
 
     private double getTotalTextHeight(FontRenderContext fontRenderContext) {
         double totalHeight = 0;
-	    for (String string : this.text) {
-	        totalHeight += this.getTextHeight(string, fontRenderContext);
+        for (String string : this.text) {
+            totalHeight += this.getTextHeight(string, fontRenderContext);
         }
 
         return totalHeight;
