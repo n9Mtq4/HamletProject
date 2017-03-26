8e892f9f90048608fcc57c4655e05701bf9dfda4
TheLocust3 <jake.kinsella@gmail.com>
2017-03-26 09:17:30 -0400

    Add skeleton for textbox and choice cutscene instructions

---
+++ src/main/java/me/willjake/hamlet/cutscene/instructions/ChoiceInstruction.java
@@ -0,0 +1,27 @@
+package me.willjake.hamlet.cutscene.instructions;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ * Display a choice and switch cutscene based off of it
+ */
+public class ChoiceInstruction extends Instruction {
+
+    private String choiceName;
+
+    public ChoiceInstruction(Node rawInstruction) {
+        super(rawInstruction);
+    }
+
+    @Override
+    public void run() {
+        // TODO: Display choice
+    }
+
+    @Override
+    public void parseInstruction(Node rawInstruction) {
+        Element element  = (Element) rawInstruction;
+        this.choiceName = element.getTextContent();
+    }
+}
