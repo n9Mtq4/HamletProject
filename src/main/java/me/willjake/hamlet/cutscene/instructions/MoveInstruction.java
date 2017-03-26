9bd2a3af427c10afa4da4def473cccb11aca3d1b
TheLocust3 <jake.kinsella@gmail.com>
2017-03-26 13:25:32 -0400

    Use a string to get the sprite for a cutscene

--- src/main/java/me/willjake/hamlet/cutscene/instructions/MoveInstruction.java
+++ src/main/java/me/willjake/hamlet/cutscene/instructions/MoveInstruction.java
@@ -7,23 +7,23 @@ import org.w3c.dom.Node;
  * Move sprite to an x and y value
  */
 public class MoveInstruction extends Instruction {
-	
-	private int x, y, spriteNumber;
-	
-	public MoveInstruction(Node rawInstruction) {
-		super(rawInstruction);
-	}
-	
-	@Override
-	public void run() {
-		// TODO: Move the sprite around
-	}
-	
-	@Override
-	public void parseInstruction(Node rawInstruction) {
-		Element element = (Element) rawInstruction;
-		this.x = Integer.parseInt(element.getAttribute("x"));
-		this.y = Integer.parseInt(element.getAttribute("x"));
-		this.spriteNumber = Integer.parseInt(element.getTextContent());
-	}
+    private int x, y;
+    private String spriteString;
+
+    public MoveInstruction(Node rawInstruction) {
+        super(rawInstruction);
+    }
+
+    @Override
+    public void run() {
+        // TODO: Move the sprite around
+    }
+
+    @Override
+    public void parseInstruction(Node rawInstruction) {
+        Element element  = (Element) rawInstruction;
+        this.x = Integer.parseInt(element.getAttribute("x"));
+        this.y = Integer.parseInt(element.getAttribute("y"));
+        this.spriteString = element.getTextContent();
+    }
 }
