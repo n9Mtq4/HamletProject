d875fd5c51d259a93589e93d8f1a9ea002435e72
n9Mtq4 <bill.wresnahan@gmail.com>
2017-03-26 20:02:01 -0400

    add fortinbras

--- src/main/java/me/willjake/hamlet/game/level/DuelEndingLevel.java
+++ src/main/java/me/willjake/hamlet/game/level/DuelEndingLevel.java
@@ -3,6 +3,7 @@ package me.willjake.hamlet.game.level;
 import me.willjake.hamlet.entity.Direction;
 import me.willjake.hamlet.game.Tiles;
 import me.willjake.hamlet.game.entity.Claudius;
+import me.willjake.hamlet.game.entity.Fortinbras;
 import me.willjake.hamlet.game.entity.Gertrude;
 import me.willjake.hamlet.game.entity.Guard;
 import me.willjake.hamlet.game.entity.Hamlet;
@@ -19,20 +20,22 @@ public class DuelEndingLevel extends Level {
 	
 	public DuelEndingLevel() {
 		super("/assets/world/greathall.png", "/assets/world/greathallfront.png");
-
+		
 		Laertes laertes = new Laertes(6, 7);
 		laertes.dir = Direction.BACKWARDS;
-
+		
 		Claudius claudius = new Claudius(5, 6);
 		claudius.dir = Direction.BACKWARDS;
-
+		
 		Gertrude gertrude = new Gertrude(7, 6);
 		gertrude.dir = Direction.BACKWARDS;
-
+		
 		Hamlet hamlet = new Hamlet(6, 9);
-
+		
 		Guard guard = new Guard(6, 5);
-
+		
+		Fortinbras fortinbras = new Fortinbras(31, 6);
+		
 		add(laertes);
 		add(claudius);
 		add(gertrude);
