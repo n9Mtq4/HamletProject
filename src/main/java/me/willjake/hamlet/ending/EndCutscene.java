1f0540c4e1bba202c75c6b4201db23133b038834
n9Mtq4 <bill.wresnahan@gmail.com>
2017-04-04 22:59:48 -0400

    end cut scene lines up

--- src/main/java/me/willjake/hamlet/ending/EndCutscene.java
+++ src/main/java/me/willjake/hamlet/ending/EndCutscene.java
@@ -3,6 +3,7 @@ package me.willjake.hamlet.ending;
 import me.willjake.hamlet.render.Display;
 
 import javax.imageio.ImageIO;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
@@ -18,8 +19,13 @@ import java.io.IOException;
  */
 public class EndCutscene {
 	
-	// this thing should run for 80 seconds or 4800 ticks
-	private static final int TICK_LIMIT = 4800;
+	// this thing should run for 81 seconds (1 min, 21 seconds)?
+	private static final int TICK_LIMIT = 81 * 60;
+	private static final int ANIMATION_SPEED = 3; // bigger = slower
+	private static final int ANIMATION_SPEED_TICK = 1; // bigger = faster
+	private static final int FADE_TIME = 4 * 60;
+	private static final int FADE_OUT_POINT = TICK_LIMIT - FADE_TIME; // bug when fadeOutVal time > 255 ticks. int truncates to fadeSpeed = 0
+	private static final int FADE_IN_STOP = FADE_TIME;
 	
 	private Display display;
 	private int time = 0;
@@ -28,12 +34,21 @@ public class EndCutscene {
 	private int frame = 0;
 	private int yOff = 0;
 	
+	private boolean fadeIn = true;
+	private int fadeInVal = 0xff;
+	private boolean fadeOut = false;
+	private int fadeOutVal = 0;
+	private int fadeSpeed = 0xff / FADE_TIME;
+	
 	public EndCutscene(Display display) {
 		this.display = display;
 	}
 	
 	public void init() {
 		
+		if (display.music != null && display.music.isRunning()) {
+			display.music.stop();
+		}
 		display.music = display.playSound("honorforall");
 		loadImages();
 		
@@ -47,11 +62,40 @@ public class EndCutscene {
 	
 	public void tick() {
 		
-		if (time > 0 && time <= 60 * 10) {
-			frame = 0;
+		if (time == FADE_IN_STOP) {
+			fadeIn = false;
 		}
 		
-		yOff--;
+		if (time < FADE_IN_STOP) {
+			fadeInVal -= fadeSpeed;
+			if (fadeInVal <= 0) fadeInVal = 0;
+		}
+		
+		// first frame
+		if (time == FADE_IN_STOP) {
+			if (display.deadPeople.get("rg") != null) {
+				if (display.deadPeople.get("rg")) frame = 0;
+			}
+			frame = 0; // TODO: this cause r & g can't live currently in this version. change it when adding r and g choice
+		} else if (time == 60 * 10 + 1) {
+			frame = 1;
+			yOff = 0;
+		}
+		
+		if (time == FADE_OUT_POINT) { // 79 * 60
+			// two second fadeOutVal out
+			fadeOut = true;
+		}
+		
+		if (time > FADE_OUT_POINT) { // 79 * 60
+			fadeOutVal += fadeSpeed;
+			if (fadeOutVal > 255) fadeOutVal = 255;
+		}
+		
+		if (time % ANIMATION_SPEED == 0) {
+			yOff -= ANIMATION_SPEED_TICK;
+		}
+		
 		time++;
 		if (time >= TICK_LIMIT) {
 			System.out.println("Warning: End is past the desired length of end scene\nSwitching to credits");
@@ -62,7 +106,23 @@ public class EndCutscene {
 	
 	public void render(Graphics g) {
 		
-		g.drawImage(frames[frame], 0, yOff, null);
+		final Color backupColor = g.getColor();
+		
+		if (fadeIn) {
+			g.setColor(new Color(fadeInVal, fadeInVal, fadeInVal)); // slowly turn white
+			g.fillRect(0, 0, Display.WIDTH * Display.SCALE, Display.HEIGHT * Display.SCALE);
+		}
+		
+		if (!fadeIn) {
+			g.drawImage(frames[frame], 0, yOff, null);
+		}
+		
+		if (fadeOut) {
+			g.setColor(new Color(0, 0, 0, fadeOutVal)); // slowly increase alpha
+			g.fillRect(0, 0, Display.WIDTH * Display.SCALE, Display.HEIGHT * Display.SCALE);
+		}
+		
+		g.setColor(backupColor);
 		
 	}
 	
--- src/main/java/me/willjake/hamlet/mainmenu/MainMenu.java
+++ src/main/java/me/willjake/hamlet/mainmenu/MainMenu.java
@@ -57,6 +57,7 @@ public class MainMenu {
 		if (musicPlaying) return;
 		musicPlaying = true;
 		music = display.playSound("mainmenu");
+		display.music = music;
 		music.loop(Clip.LOOP_CONTINUOUSLY);
 	}
 	
