3c3d931c2ab2400567c8411aa649b88e8ce843b6
n9Mtq4 <bill.wresnahan@gmail.com>
2017-04-04 22:24:48 -0400

    r and g end scene

--- src/main/java/me/willjake/hamlet/ending/EndCutscene.java
+++ src/main/java/me/willjake/hamlet/ending/EndCutscene.java
@@ -2,7 +2,12 @@ package me.willjake.hamlet.ending;
 
 import me.willjake.hamlet.render.Display;
 
+import javax.imageio.ImageIO;
 import java.awt.Graphics;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Created by will on 4/2/17 at 10:15 PM.
@@ -19,6 +24,10 @@ public class EndCutscene {
 	private Display display;
 	private int time = 0;
 	
+	private BufferedImage[] frames = new BufferedImage[10]; // change this size to the actual size
+	private int frame = 0;
+	private int yOff = 0;
+	
 	public EndCutscene(Display display) {
 		this.display = display;
 	}
@@ -26,13 +35,24 @@ public class EndCutscene {
 	public void init() {
 		
 		display.music = display.playSound("honorforall");
+		loadImages();
+		
+	}
+	
+	private void loadImages() {
+		
+		frames[0] = upscaleImage(loadImage("rg_dead"), 5.7d);
 		
 	}
 	
 	public void tick() {
 		
-		time++;
+		if (time > 0 && time <= 60 * 10) {
+			frame = 0;
+		}
 		
+		yOff--;
+		time++;
 		if (time >= TICK_LIMIT) {
 			System.out.println("Warning: End is past the desired length of end scene\nSwitching to credits");
 			display.initCredits();
@@ -40,10 +60,30 @@ public class EndCutscene {
 		
 	}
 	
-	public void render(Graphics graphics) {
+	public void render(Graphics g) {
 		
+		g.drawImage(frames[frame], 0, yOff, null);
 		
-		
+	}
+	
+	private static BufferedImage loadImage(String name) {
+		try {
+			return ImageIO.read(EndCutscene.class.getResourceAsStream("/assets/art/" + name + ".png"));
+		}catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static BufferedImage upscaleImage(BufferedImage before, double scale) {
+		int w = before.getWidth();
+		int h = before.getHeight();
+		BufferedImage after = new BufferedImage((int) (w * scale), (int) (h * scale), BufferedImage.TYPE_INT_ARGB);
+		AffineTransform at = new AffineTransform();
+		at.scale(scale, scale);
+		AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		after = scaleOp.filter(before, after);
+		return after;
 	}
 	
 }
