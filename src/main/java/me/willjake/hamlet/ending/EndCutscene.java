d95a003db69162aefefd9097099e2735ba087340
n9Mtq4 <bill.wresnahan@gmail.com>
2017-04-05 01:54:34 -0400

    it finally works!

--- src/main/java/me/willjake/hamlet/ending/EndCutscene.java
+++ src/main/java/me/willjake/hamlet/ending/EndCutscene.java
@@ -12,9 +12,9 @@ import java.io.IOException;
 
 /**
  * Created by will on 4/2/17 at 10:15 PM.
- * 
+ *
  * Check in on the characters and see how they are doing.
- * 
+ *
  * @author Will "n9Mtq4" Bresnahan
  */
 public class EndCutscene {
@@ -24,8 +24,12 @@ public class EndCutscene {
 	private static final int ANIMATION_SPEED = 3; // bigger = slower
 	private static final int ANIMATION_SPEED_TICK = 1; // bigger = faster
 	private static final int FADE_TIME = 4 * 60;
-	private static final int FADE_OUT_POINT = TICK_LIMIT - FADE_TIME; // bug when fadeOutVal time > 255 ticks. int truncates to fadeSpeed = 0
-	private static final int FADE_IN_STOP = FADE_TIME;
+	
+	private static final int FADE_IN_DEFAULT_VALUE = 0x00;
+	private static final int FADE_OUT_DEFAULT_VALUE = 0x0;
+	
+	private static final int TICKS_PER_IMAGE = (int) ((((228 * 5.7) * ANIMATION_SPEED) / ANIMATION_SPEED_TICK) - (288 * 5.7));
+	private static final int TICKS_PER_IMAGE_FADE = TICKS_PER_IMAGE + 2 * FADE_TIME;
 	
 	private Display display;
 	private int time = 0;
@@ -35,10 +39,16 @@ public class EndCutscene {
 	private int yOff = 0;
 	
 	private boolean fadeIn = true;
-	private int fadeInVal = 0xff;
+	private int fadeInVal = FADE_IN_DEFAULT_VALUE;
+	private int fadeInTime = 0;
+	private boolean badWorkaround = false;
+	
 	private boolean fadeOut = false;
-	private int fadeOutVal = 0;
-	private int fadeSpeed = 0xff / FADE_TIME;
+	private int fadeOutVal = FADE_OUT_DEFAULT_VALUE;
+	private int fadeOutTime = 0;
+	private boolean badWorkaround1 = false;
+	
+	private int fadeSpeed = 1;
 	
 	public EndCutscene(Display display) {
 		this.display = display;
@@ -57,41 +67,75 @@ public class EndCutscene {
 	private void loadImages() {
 		
 		frames[0] = upscaleImage(loadImage("rg_dead"), 5.7d);
+		frames[1] = frames[0];
 		
 	}
 	
 	public void tick() {
 		
-		if (time == FADE_IN_STOP) {
+		// stop the fade in?
+		if (fadeInTime == FADE_TIME) {
+			fadeInTime = 0;
 			fadeIn = false;
+			fadeInVal = FADE_IN_DEFAULT_VALUE;
 		}
 		
-		if (time < FADE_IN_STOP) {
-			fadeInVal -= fadeSpeed;
+		// stop the fade out?
+		if (fadeOutTime == FADE_TIME) {
+			fadeOutTime = 0;
+			fadeOut = false;
+			fadeOutVal = FADE_OUT_DEFAULT_VALUE;
+		}
+		
+		// fade in going logic
+		if (fadeIn) {
+			fadeInTime++;
+			fadeInVal += fadeSpeed;
 			if (fadeInVal <= 0) fadeInVal = 0;
 		}
 		
-		// first frame
-		if (time == FADE_IN_STOP) {
+		// fade out going logic
+		if (fadeOut) {
+			fadeOutTime++;
+			fadeOutVal += fadeSpeed;
+			if (fadeOutVal >= 255) fadeOutVal = 255;
+		}
+		
+		// frame transition times
+		// fade out
+		if ((time - (FADE_TIME)) % TICKS_PER_IMAGE_FADE == 0) {
+			if (badWorkaround){
+				fadeOut = true;
+			}else badWorkaround = true;
+		}
+		// fade in
+		if ((time - (FADE_TIME)) % TICKS_PER_IMAGE_FADE == FADE_TIME) {
+			if (badWorkaround1){
+				fadeIn = true;
+			}else badWorkaround1 = true;
+		}
+		
+		// the different points for things to happen:
+		if (time == 0) {
+			// first fade in
+			fadeIn = true;
+		}else if (time == FADE_TIME) {
+			// first frame
 			if (display.deadPeople.get("rg") != null) {
 				if (display.deadPeople.get("rg")) frame = 0;
 			}
 			frame = 0; // TODO: this cause r & g can't live currently in this version. change it when adding r and g choice
-		} else if (time == 60 * 10 + 1) {
-			frame = 1;
 			yOff = 0;
+		}else if (time == (FADE_TIME + (1 * TICKS_PER_IMAGE_FADE))) {
+			// second frame
 		}
 		
-		if (time == FADE_OUT_POINT) { // 79 * 60
-			// two second fadeOutVal out
+		// last, fade out
+		if (time == TICK_LIMIT - FADE_TIME) {
 			fadeOut = true;
 		}
 		
-		if (time > FADE_OUT_POINT) { // 79 * 60
-			fadeOutVal += fadeSpeed;
-			if (fadeOutVal > 255) fadeOutVal = 255;
-		}
-		
+		// tick the animation of scrolling images
 		if (time % ANIMATION_SPEED == 0) {
 			yOff -= ANIMATION_SPEED_TICK;
 		}
